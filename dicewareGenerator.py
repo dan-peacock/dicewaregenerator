598ad77fc22f2078af4299c0c4ba11b236430b23
Daniel Peacock <daniel.peacock@citrix.com>
2019-04-17 12:55:28 +0100

    Add files to repo.

--- dicewareGenerator.py
+++ dicewareGenerator.py
@@ -10,7 +10,14 @@ class DicewarePasswordGenerator:
         self.number_of_words = int(input("How many diceware words do you want to use? ")) 
         for x in range(self.number_of_words):
             diceware_number = input(f"Enter dice number sequence #{x+1}: ")
-            self.generated_numbers.append(diceware_number)
+            if len(diceware_number)>5:
+                print("Please enter a 5 digit number.")
+                diceware_number = input(f"Enter dice number sequence #{x+1}: ")
+            else:
+                self.generated_numbers.append(diceware_number)
+
+    def download_diceware_list(self):
+        pass
 
     def find_diceware_words(self):
         wordlist = open('C:/Users/danielpeac/Desktop/diceware/diceware.wordlist.asc', 'r')
@@ -21,16 +28,17 @@ class DicewarePasswordGenerator:
                     self.generated_password.append(diceware_word)
 
     def print_password(self):
+        print("Your password is:", end=" ")
         for x in self.generated_password:
             print(x, end=" ")
 
     def calculate_entropy(self):
-        length = len(self.generated_password)
-        possible = 7776^length
-        password_entropy = math.log2(possible)
-        total = password_entropy*self.number_of_words
-        print()
-        print(f"The entropy of your password is {total}")
+        sum = 0
+        for x in range(len(self.generated_password)):   
+            sum += len(self.generated_password[x])
+        password_length = sum + (len(self.generated_password) - 1)
+        password_entropy = math.log2(7776**password_length)
+        print(f"\nThe entropy of your password is {password_entropy}")
 
 DicewarePasswordGenerator = DicewarePasswordGenerator()
 DicewarePasswordGenerator.take_user_input()

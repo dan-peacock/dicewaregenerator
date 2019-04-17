7fa013797e03bef3482513bba24a2f622edb7389
Daniel Peacock <daniel.peacock@citrix.com>
2019-04-17 15:53:34 +0100

    Add relative paths for download_diceware_list and find_diceware_words

--- dicewareGenerator.py
+++ dicewareGenerator.py
@@ -1,11 +1,13 @@
 import math
 import requests
+import os
 
 class DicewarePasswordGenerator:
     def __init__(self):
         self.number_of_words = 0
         self.generated_numbers = []
         self.generated_password = []
+        self.path = os.path.dirname(__file__)
     
     def take_user_input(self):
         self.number_of_words = int(input("How many diceware words do you want to use? ")) 
@@ -20,10 +22,11 @@ class DicewarePasswordGenerator:
     def download_diceware_list(self):
         url = 'http://world.std.com/~reinhold/diceware.wordlist.asc'
         r = requests.get(url, allow_redirects=True)
-        open('diceware.wordlist.asc', 'wb').write(r.content)
+        open('diceware2.wordlist.asc', 'wb').write(r.content)
 
     def find_diceware_words(self):
-        wordlist = open('C:/Users/danielpeac/git repos/dicewarediceware.wordlist.asc', 'r')
+        self.finalpath = self.path+'/diceware2.wordlist.asc'
+        wordlist = open(self.finalpath, 'r')
         for line in wordlist:
             for x in self.generated_numbers:
                 if x in line:
@@ -42,9 +45,3 @@ class DicewarePasswordGenerator:
         password_length = sum + (len(self.generated_password) - 1)
         password_entropy = math.log2(7776**password_length)
         print(f"\nThe entropy of your password is {password_entropy}")
-
-DicewarePasswordGenerator = DicewarePasswordGenerator()
-DicewarePasswordGenerator.take_user_input()
-DicewarePasswordGenerator.find_diceware_words()
-DicewarePasswordGenerator.print_password()
-DicewarePasswordGenerator.calculate_entropy()
